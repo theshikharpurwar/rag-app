4282cee2051587d8b727c00a480b3289524656ac
Shikhar Purwar <spurwar2002@gmail.com>
2025-02-20 23:19:07 +0530

    refactor: simplify MongoDB connection code in server.js

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,7 @@ app.use(express.json());
 
 app.use('/api', routes);
 
-mongoose.connect('mongodb://localhost:27017/rag-app', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect('mongodb://localhost:27017/rag-app')
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
